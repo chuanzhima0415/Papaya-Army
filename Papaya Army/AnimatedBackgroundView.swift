52f6d456215388e62e948bedd5a0d5422c7d787c
ChuanZhiMa0415 <chuanzhima0415@gmail.com>
2025-05-10 10:22:31 +0800

    修改了一下 mesh gradient animation 的参数

--- Papaya Army/AnimatedBackgroundView.swift
+++ Papaya Army/AnimatedBackgroundView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct AnimatedBackgroundView: View {
 	@State private var colors = MeshGradientData.randomColors()
-	private let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
+	private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
 	var body: some View {
 		MeshGradient(width: 4, height: 4, points: MeshGradientData.points, colors: colors)
 			.onReceive(timer) { _ in
-				withAnimation(.easeInOut(duration: 5)) {
+				withAnimation(.easeInOut(duration: 3).repeatForever()) {
 					colors = MeshGradientData.randomColors()
 				}
 			}
